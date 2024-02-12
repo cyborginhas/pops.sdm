f147596f25a11b08b8ae18ebb7670c0c28283db9
cyborginhas <67606742+cyborginhas@users.noreply.github.com>
2024-02-12 12:32:54 -0500

    remove unnecessary check

--- sdm_checks.R
+++ sdm_checks.R
@@ -10,12 +10,12 @@
 #' @export
 
 check_resolution <- function(country, res) {
-  if (country == "world" && res < 1000) {
+  if (domain == "global" && res < 1000) {
     print("Resolution must be >= 1000m for global analysis. Returning 1000m base
     raster.")
     res <- 1000
-  } else if (country != "world" && res < 30) {
-    print("Resolution must be >= 30m for country analysis. Returning 30m base 
+  } else if (domain != "conus" && res < 30) {
+    print("Resolution must be >= 30m for L48 analysis. Returning 30m base 
     raster.")
     res <- 30
   } else {
@@ -23,22 +23,3 @@ check_resolution <- function(country, res) {
   }
   return(res)
 }
-
-#' @description This function checks if the country provided by the user is a
-#' valid ISO-3 code. If the country is not a valid ISO-3 code, the function
-#' returns an error.
-#' @param country The ISO-3 code for the country to create the base raster for;
-#' if left NULL, it is assumed that the analysis is global.
-#' @export
-
-check_country <- function(country) {
-  if (nchar(country) > 3) {
-    stop("Country is not a valid ISO-3 code.")
-  } else if (is.null(country)) {
-    print("No country ISO-3 provided. Assuming global analysis.")
-  } else {
-    country <- country
-  }
-  return(country)
-}
-
