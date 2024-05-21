d93c2f2eb99324fd9f963060e75d628797369099
cyborginhas <67606742+cyborginhas@users.noreply.github.com>
2024-05-21 11:56:48 -0400

    various prep functions for run_sdm.R

--- sdm_helpers.R
+++ sdm_helpers.R
@@ -38,3 +38,46 @@ format_species_name <- function(species) {
   species <- paste0(toupper(substr(species, 1, 1)), substr(species, 2, nchar(species)))
   return(species)
 }
+
+#' @description This function uses the species name provided by format_species_name
+#' and the path to the species occurrence data to read in the species occurrence
+#' data. The function reads in the species occurrence data and returns a data
+#' table with the species occurrence data.
+#' @param species The species name returned by format_species_name.
+#' @param path The path to the species occurrence data.
+
+
+get_species_occurrences <- function(species, path) {
+  species <- format_species_name(species)
+  files <- list.files(paste0(path, "/Table/"), pattern = species, full.names = TRUE, recursive = TRUE)
+  occs <- lapply(files, function(x) {
+    fread(x, colClasses = "character")
+  })
+  occs <- rbindlist(occs, fill = TRUE)
+  occs[, `:=`(lat = as.numeric(lat), lon = as.numeric(lon), date = as.numeric(date), p_a = as.numeric(p_a))]
+  occs <- occs[date > 1990]
+  #occs <- occs[p_a > 0]
+  return(occs)
+}
+
+#' @description After reading in the species occurrence data, this function
+#' crops the species occurrence data to the domain of interest. The function
+#' returns the cropped species occurrence data.
+#' @param species The species name returned by format_species_name.
+#' @param path The path to the species occurrence data. Should point to the
+#' main Data folder.
+#' @param extent The SpatVector object representing the domain of
+#' interest.
+
+crop_species_occurrences <- function(species, path, extent) {
+  occs <- get_species_occurrences(species, path)
+  occs_pts <- vect(occs, crs = crs(extent), geom = c("lon", "lat"))
+  extent <- vect(extent)
+  extent <- project(extent, crs(occs_pts))
+  occs_extent <- crop(occs_pts, extent)
+  occs_extent$lon <- crds(occs_extent)[, 1]
+  occs_extent$lat <- crds(occs_extent)[, 2]
+  occs_extent <- as.data.table(occs_extent)
+}
+
+#' @description This function
