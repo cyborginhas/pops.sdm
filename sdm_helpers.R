179fcbf556ffb26723f351dcd815084af43b4844
cyborginhas <67606742+cyborginhas@users.noreply.github.com>
2024-05-26 16:51:41 -0400

    add in function to create random background points, thickened background points

--- sdm_helpers.R
+++ sdm_helpers.R
@@ -43,7 +43,10 @@ format_species_name <- function(species) {
   # ensure the first letter is in uppercase and the rest are in lowercase
   species <- tolower(species)
   # make first letter uppercase
-  species <- paste0(toupper(substr(species, 1, 1)), substr(species, 2, nchar(species)))
+  species <- paste0(
+    toupper(substr(species, 1, 1)),
+    substr(species, 2, nchar(species))
+  )
   return(species)
 }
 
@@ -61,40 +64,65 @@ crop_predictor <- function(
     pred_copies_path,
     pred_cropped_path,
     extent) {
-  s <- Sys.time()
-  # Copy the file to the predictors directory
-  file.copy(file, pred_copies_path)
-  copy <- paste0(pred_copies_path, basename(file))
-  r <- terra::rast(copy)
-  # Replace the path with the new path
-  new_filename <- paste0(pred_cropped_path, basename(copy))
-  # Add cropped to the filename
-  new_filename <- gsub(".tif", "_cropped.tif", new_filename)
-  dtype <- terra::datatype(r)
-  # Crop the raster to the study extent
-  terra::crop(r, extent,
-    filename = new_filename,
-    wopt = list(
-      gdal = "COMPRESS=ZSTD",
-      datatype = dtype, overwrite = TRUE
+  # Check if the file exists, if it does not, copy, crop & zscore
+  cropped_file <- paste0(pred_cropped_path, basename(file))
+  cropped_file <- gsub(".tif", "_cropped.tif", cropped_file)
+  zscore_file <- gsub("/cropped/", "/cropped/transformed/", cropped_file)
+  zscore_file <- gsub(".tif", "_zscore.tif", zscore_file)
+
+  if (!file.exists(cropped_file)) {
+    print(paste0("Cropping ", basename(file)))
+    s <- Sys.time()
+    # Copy the file to the predictors directory
+    file.copy(file, pred_copies_path)
+    copy <- paste0(pred_copies_path, basename(file))
+    r <- terra::rast(copy)
+    # Replace the path with the new path
+    new_filename <- paste0(pred_cropped_path, basename(copy))
+    # Add cropped to the filename
+    new_filename <- gsub(".tif", "_cropped.tif", new_filename)
+    dtype <- terra::datatype(r)
+    # Crop the raster to the study extent
+    terra::crop(r, extent,
+      filename = new_filename,
+      wopt = list(
+        gdal = "COMPRESS=ZSTD",
+        datatype = dtype, overwrite = TRUE
+      )
     )
-  )
-  cropped_raster <- terra::rast(new_filename)
-  # Throw an error if copy does not contain the correct path
-  if (!grepl("1_Inputs/2_Predictors/1_Current/copies", copy)) {
-    stop("Attemping to delete a file that is not in the correct directory.")
+    cropped_raster <- terra::rast(new_filename)
+    # Throw an error if copy does not contain the correct path
+    if (!grepl("1_Inputs/2_Predictors/1_Current/copies", copy)) {
+      stop("Attemping to delete a file that is not in the correct directory.")
+    }
+    # Change file access permissions
+    Sys.chmod(copy, mode = "0777")
+    # Remove the file
+    file.remove(copy)
+    t <- Sys.time() - s
+    print(paste0(
+      "Time taken to crop ", basename(new_filename), ": ",
+      t, attr(t, "units")
+    ))
+    if (dtype != "INT1U") {
+      zscore_raster <- spatialEco::raster.transformation(
+        cropped_raster,
+        trans = "std"
+      )
+    } else {
+      zscore_raster <- cropped_raster
+    }
+    terra::tmpFiles(orphan = TRUE, old = TRUE, remove = TRUE)
+  } else {
+    cropped_raster <- terra::rast(cropped_file)
+    dtype <- terra::datatype(cropped_raster)
+    if (dtype != "INT1U") {
+      zscore_raster <- terra::rast(zscore_file)
+    } else {
+      zscore_raster <- cropped_raster
+    }
   }
-  # Change file access permissions
-  Sys.chmod(copy, mode = "0777")
-  # Remove the file
-  file.remove(copy)
-  t <- Sys.time() - s
-  print(paste0(
-    "Time taken to crop ", basename(new_filename), ": ",
-    t, attr(t, "units")
-  ))
-  terra::tmpFiles(orphan = TRUE, old = TRUE, remove = TRUE)
-  return(cropped_raster)
+  return(zscore_raster)
 }
 
 #' @description Function to crop species occurrence data to a specified extent
@@ -140,25 +168,34 @@ prep_occurrences <- function(occs, extent, year = NULL) {
 #' @note Will add option to specify resolutions beyond 30m, 100m, 250m, 500m, 1000m
 #' in future versions.
 
-cropped_base_raster <- function(domain, res, path, extent) {
-  # Retrieve base raster
-  if (domain == "USA") {
-    common_res_names <- c(30, 100, 250, 500, 1000)
-    base <- base_conus(path)
-    # Pull the base raster at the specified resolution
-    base <- base[[which(common_res_names == res)]]
-  } else if (domain == "Global") {
-    common_res_names <- c(1000, 2500, 5000)
-    base <- base_global(path)
-    # Pull the base raster at the specified resolution
-    base <- base[[which(common_res_names == res)]]
+crop_base_raster <- function(domain, res, path, extent) {
+  cropped_base_path <- paste0(getwd(), "/flexsdm_results/1_Inputs/3_Calibration_area/base_study_extent.tif") # no lint
+  if (!file.exists(cropped_base_path)) {
+    # Retrieve base raster
+    if (domain == "USA") {
+      common_res_names <- c(30, 100, 250, 500, 1000)
+      base <- base_conus(path)
+      # Pull the base raster at the specified resolution
+      base <- base[[which(common_res_names == res)]]
+    } else if (domain == "Global") {
+      common_res_names <- c(1000, 2500, 5000)
+      base <- base_global(path)
+      # Pull the base raster at the specified resolution
+      base <- base[[which(common_res_names == res)]]
+    } else {
+      stop("Domain must be 'USA' or 'Global'")
+    }
+    # Crop the base raster to the extent of interest
+    extent <- terra::vect(extent)
+    extent <- terra::project(extent, y = terra::crs(base))
+    base <- terra::crop(base, extent)
+    terra::writeRaster(base, cropped_base_path,
+      overwrite = TRUE,
+      gdal = "COMPRESS=ZSTD", datatype = "INT1U"
+    )
   } else {
-    stop("Domain must be 'USA' or 'Global'")
+    base <- terra::rast(cropped_base_path)
   }
-  # Crop the base raster to the extent of interest
-  extent <- terra::vect(extent)
-  extent <- terra::project(extent, y = terra::crs(base))
-  base <- terra::crop(base, extent)
   return(base)
 }
 
@@ -224,9 +261,9 @@ geo_filter_occs <- function(occs, extent, d, species) {
     x = "x",
     y = "y",
     env_layer = extent,
-    method = c("defined", d = d/1000)
+    method = c("defined", d = d / 1000)
   )
-
+  filt_geo$d <- d
   # Write out the filtered data
   occsfilt_path <- paste0(
     getwd(),
@@ -242,52 +279,200 @@ geo_filter_occs <- function(occs, extent, d, species) {
   return(filt_geo)
 }
 
-#' @description Function to partition the data using spatial
-#' block cross-validation, and the inputs from geo_filter_occs.
-#' The function returns the partitioned data, and a raster of
-#' the spatial blocks.
+#' @description Function to pull out the predictors that are categorical
+#' and create a list of the integer and continuous predictors.
+#' @param predictor A predictor to be used in the analysis.
+
+get_numerical_rasters <- function(predictor) {
+  dt <- terra::datatype(predictor)
+  if (dt != "INT1U") {
+    predictor <- predictor
+  } else {
+    predictor <- NULL
+  }
+  return(predictor)
+}
+
+#' @description Function to partition the data using  environmenal
+#' and spatial cross-validation, and the inputs from geo_filter_occs.
+#' The function returns the partitionned data.
 #' @param occs The filtered species occurrence data.
-#' @param extent The base raster cropped to the extent of interest.
-#' @param npart The number of partitions to create.
-#' @param res The resolution of the analysis.
+#' @param env_layer The list of predictors.
 
-## npart <- 5
-## extent <- base
-## occs <- filt_geo[[1]]
-## occs$id <- 1:nrow(occs)
+spatial_block_partition <- function(occs, extent, env_layer, species) {
+  s <- Sys.time()
+  # Partition the data
+  part_data <- flexsdm::part_sblock(
+    env_layer = env_layer,
+    data = occs,
+    x = "x",
+    y = "y",
+    pr_ab = "pr_ab",
+    n_part = 5,
+    min_res_mult = 3,
+    max_res_mult = 300,
+    num_grids = 45,
+    prop = 0.75,
+    min_occ = 10
+  )
+
+  # Write out the partitioned data
+  # Create outpath
+  part_path <- paste0(
+    getwd(),
+    "/flexsdm_results/1_Inputs/1_Occurrences/partitioned/"
+  )
+  dir.create(part_path, showWarnings = FALSE, recursive = TRUE)
+
+  # Write out the partitioned data
+  names(part_data) <- paste0(names(part_data), "_filtgeo_", d, "m2")
+  d <- unique(occs$d)
+  part_data[[2]]$d <- d
+  # Best partition
+  data.table::fwrite(part_data[[2]], paste0(
+    part_path, species, "_best_part_filtgeo", d, "m2.csv"
+  ))
+  # Partition data
+  part_data[[1]]$d <- d
+  data.table::fwrite(part_data[[1]], paste0(
+    part_path, species, "_part_data_filtgeo", d, "m2.csv"
+  ))
+  names(part_data[[3]]) <- paste0(names(part_data[[3]]), "_filtgeo_", d, "m2")
+  # Partition grid
+  terra::writeRaster(part_data[[3]], paste0(
+    part_path, species, "_part_grid_filtgeo", d, "m2.tif"
+  ), overwrite = TRUE, datatype = "INT1U", gdal = "COMPRESS=ZSTD")
+
+  t <- Sys.time() - s
+  print(paste0(
+    "Time taken to partition ", length(occs$x),
+    " points: ", t, attr(t, "units")
+  ))
+  return(part_data)
+}
+
+#' @description A function to create randomly sampled background points data
+#' and write the data to the local output path.
+#' @param partitioned_data A list of partitioned data occurrences, and the
+#' associated spatial block grid.
+#' @param species The species name.
+#' associated grid.
+
+create_random_bg_pts <- function(partitioned_data, species) {
+  occs_p <- partitioned_data[[1]]
+  grid_env <- partitioned_data[[3]]
+  d <- unique(occs_p$d)
+
+  bg_pts <- flexsdm::sample_background(
+    data = occs_p,
+    x = "x",
+    y = "y",
+    n = length(occs_p$x),
+    method = "random",
+    rlayer = grid_env
+  )
+
+  # Extract grid values
+  part <- terra::extract(grid_env, bg_pts[, c("x", "y")], ID = FALSE)
+  part <- part[[1]]
+  bg_pts$.part <- part
+
+  # Combine the background points with the occurrences
+  occs_p <- data.table::rbindlist(list(occs_p, bg_pts),
+    use.names = TRUE,
+    fill = TRUE
+  )
+  occs_p$d <- d
+
+  # Write out the background points
+  bg_path <- paste0(
+    getwd(),
+    "/flexsdm_results/1_Inputs/1_Occurrences/background/random/"
+  )
+  dir.create(bg_path, showWarnings = FALSE, recursive = TRUE)
+
+  data.table::fwrite(occs_p, paste0(
+    bg_path, species, "_bg_pts_filtgeo_", d, "m2.csv"
+  ), row.names = FALSE)
+  return(occs_p)
+}
 
 
-## partition_data <- function(occs, npart, res) {
-##   sp_part3 <- part_sblock(
-##     env_layer = extent,
-##     data = occs,
-##     x = "x",
-##     y = "y",
-##     pr_ab = "pr_ab",
-##     min_res_mult = 10, # Minimum value used for multiplying raster resolution and define the finest resolution to be tested
-##     max_res_mult = 100, # Maximum value used for multiplying raster resolution and define the coarsest resolution to be tested
-##     num_grids = 5, # Number of grid to be tested between min_res_mult X (raster resolution) and max_res_mult X (raster resolution)
-##     n_part = npart, # Number of partitions
-##     prop = 0.5, # Proportion of points used for testing autocorrelation between groups (0-1)
-##     min_occ = floor(length(occs) / (npart * 2)) # Minimum number of occurrences to be used in each partition
-##   )
+#' @description A function to create thickened background points data
+#' and write the data to the local output path.
+#' @param partitioned_data A list of partitioned data occurrences, and the
+#' associated spatial block grid.
+#' @param base The base raster cropped to the extent of interest.
+#' @param res The resolution of the base raster.
+#' @param species The species name.
+#' associated grid.
 
-##   grid_env <- get_block(env_layer = extent, best_grid = sp_part3$grid)
-##   return(list(sp_part3, grid_env))
-## }
+create_thickened_bg_pts <- function(
+    partitioned_data,
+    base, res, species) {
+  s <- Sys.time()
+  occs_p <- partitioned_data[[1]]
+  grid_env <- partitioned_data[[2]]
 
-## sp_part3 <- part_sblock(
-##   env_layer = base_raster,
-##   data = filt_geo[[1]],
-##   x = "x",
-##   y = "y",
-##   pr_ab = "p_a",
-##   min_res_mult = 10, # Minimum value used for multiplying raster resolution and define the finest resolution to be tested
-##   max_res_mult = 1000, # Maximum value used for multiplying raster resolution and define the coarsest resolution to be tested
-##   num_grids = 50, # Number of grid to be tested between min_res_mult X (raster resolution) and max_res_mult X (raster resolution)
-##   n_part = n_parts, # Number of partitions
-##   prop = 0.5, # Proportion of points used for testing autocorrelation between groups (0-1)
-##   min_occ = floor(length(filt_geo$fkey)/(no_parts*2)) # Minimum number of occurrences to be used in each partition
-## )
+  # Disaggregate the spatial block grid
 
-## grid_env <- get_block(env_layer = base_raster, best_grid = sp_part3$grid)
+  if (res == 30) {
+    factor <- floor(terra::res(grid_env)[1] / terra::res(base)[1] / 5)
+    grid_env <- terra::disagg(grid_env, factor)
+  } else {
+    grid_env <- terra::resample(grid_env, base, method = "near")
+  }
+
+  d <- unique(occs_p$d)
+  no_parts <- length(unique(occs_p$.part))
+
+  part_pts <- list()
+  bg_pts <- list()
+  grid_parts <- list()
+
+  for (i in 1:no_parts) {
+    s <- Sys.time()
+    part_pts[[i]] <- occs_p[occs_p$.part == i, ]
+    grid_parts[[i]] <- terra::ifel(grid_env == i, 1, NA)
+    bg_pts[[i]] <- flexsdm::sample_background(
+      data = part_pts[[i]],
+      x = "x",
+      y = "y",
+      n = length(part_pts[[i]]$x),
+      method = c("thickening", width = 10000),
+      rlayer = grid_parts[[i]],
+      maskval = 1
+    )
+    bg_pts[[i]]$.part <- i
+    t <- Sys.time() - s
+    print(paste0(
+      "Time taken to create background points for part ", i, ": ",
+      t, " ", attr(t, "units")
+    ))
+  }
+
+  bg_pts <- data.table::rbindlist(bg_pts, use.names = TRUE, fill = TRUE)
+  # Combine the background points with the occurrences
+  occs_p <- data.table::rbindlist(list(occs_p, bg_pts),
+    use.names = TRUE,
+    fill = TRUE
+  )
+  occs_p$d <- d
+
+  # Write out the background points
+  bg_path <- paste0(
+    getwd(),
+    "/flexsdm_results/1_Inputs/1_Occurrences/background/thicken/"
+  )
+  dir.create(bg_path, showWarnings = FALSE, recursive = TRUE)
+
+  data.table::fwrite(occs_p, paste0(
+    bg_path, species, "_bg_pts_filtgeo_", d, "m2.csv"
+  ), row.names = FALSE)
+  t <- Sys.time() - s
+  print(paste0(
+    "Time taken to create background points: ", t, " ", attr(t, "units"),
+    " for ", length(occs_p$x), " points for filtgeo", d, "m2"
+  ))
+  return(occs_p)
+}
