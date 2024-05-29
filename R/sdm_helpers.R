da03848d097d48ea2b581d0882f1f65ff79f4d41
cyborginhas <67606742+cyborginhas@users.noreply.github.com>
2024-05-29 11:51:32 -0400

    removing old workflow

--- R/sdm_helpers.R
+++
@@ -1,9 +0,0 @@
-#' @export
-
-load_raster <- function(drive_path, fact, res) {
-  base.rast <- terra::rast(drive_path)
-  base.rast <- terra::crop(base.rast, terra::ext(pops.sdm::l48()))
-  base.rast <- terra::aggregate(x = base.rast, fact = fact)
-  base.rast <- base.rast / base.rast
-  terra::writeRaster(base.rast, paste(geodir, 'USA\\base_', res, 'm.tif', sep=''))
-}
