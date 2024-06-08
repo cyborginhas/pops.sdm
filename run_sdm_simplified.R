fe5116b382ffc30e9f5a726c9b659c1ddc04f767
cyborginhas <67606742+cyborginhas@users.noreply.github.com>
2024-06-08 16:23:33 -0400

    update filename

---
+++ run_sdm_simplified.R
@@ -0,0 +1,38 @@
+#' Run the species distribution model: currently runs for Maxent only. Will add in other models later.
+
+#' 1. Load the required packages & set path
+library(flexsdm)
+library(terra)
+library(data.table)
+library(parallel)
+library(spatialEco)
+
+
+#' Prep data for maxent
+tg_pts_wdata <- "C:/Users/blaginh/Desktop/maximize/Ailanthus_altissima_target_group_bg_pts_filtgeo_210m2_wpreds.csv"
+tg_pts_wdata <- fread(tg_pts_wdata)
+occs <- splitpts4sdm(tg_pts_wdata)
+
+max_t1 <- readRDS("C:/Users/blaginh/Desktop/maximize/Ailanthus_altissima_maxent.rds")
+tiles <- list.files("C:/Users/blaginh/Desktop/maximize/tiles/", pattern = ".tif", full.names = TRUE)
+tiles <- lapply(tiles, terra::rast)
+basename(sources(tiles[[1]]))
+
+# a list of models
+for (i in seq_along(tiles)) {
+    s <- Sys.time()
+    ind_p <- sdm_predict(
+    nchunk = 1,
+    models = max_t1,
+    pred = tiles[[i]],
+    thr = c("max_sens_spec"),
+    con_thr = FALSE,
+    predict_area = NULL
+  )
+  fn <- basename(sources(tiles[[i]]))
+  writeRaster(ind_p$max, paste0("C:/Users/blaginh/Desktop/maximize/Ailanthus_altissima_prediction",fn))
+  t <- Sys.time() - s
+  print(t)
+  tmpFiles(orphan = TRUE, old = TRUE, remove = TRUE)
+}
+
