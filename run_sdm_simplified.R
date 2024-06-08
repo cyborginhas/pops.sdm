77ec8f2fa84149b254ca751b5d171684717b626e
cyborginhas <67606742+cyborginhas@users.noreply.github.com>
2024-06-08 16:37:29 -0400

    add trycatch to simplified sdm run

--- run_sdm_simplified.R
+++ run_sdm_simplified.R
@@ -14,25 +14,29 @@ tg_pts_wdata <- fread(tg_pts_wdata)
 occs <- splitpts4sdm(tg_pts_wdata)
 
 max_t1 <- readRDS("C:/Users/blaginh/Desktop/maximize/Ailanthus_altissima_maxent.rds")
-tiles <- list.files("C:/Users/blaginh/Desktop/maximize/tiles/", pattern = ".tif", full.names = TRUE)
+tiles <- list.files("C:/Users/blaginh/Desktop/maximize/tiles/tiles/", pattern = ".tif", full.names = TRUE)
 tiles <- lapply(tiles, terra::rast)
-basename(sources(tiles[[1]]))
 
 # a list of models
 for (i in seq_along(tiles)) {
+  tryCatch({
     s <- Sys.time()
     ind_p <- sdm_predict(
-    nchunk = 1,
-    models = max_t1,
-    pred = tiles[[i]],
-    thr = c("max_sens_spec"),
-    con_thr = FALSE,
-    predict_area = NULL
-  )
-  fn <- basename(sources(tiles[[i]]))
-  writeRaster(ind_p$max, paste0("C:/Users/blaginh/Desktop/maximize/Ailanthus_altissima_prediction",fn))
-  t <- Sys.time() - s
-  print(t)
-  tmpFiles(orphan = TRUE, old = TRUE, remove = TRUE)
+      nchunk = 1,
+      models = max_t1,
+      pred = tiles[[i]],
+      thr = c("max_sens_spec"),
+      con_thr = FALSE,
+      predict_area = NULL
+    )
+    fn <- basename(sources(tiles[[i]]))
+    writeRaster(ind_p$max, paste0("C:/Users/blaginh/Desktop/maximize/output/Ailanthus_altissima_prediction_",fn))
+    t <- Sys.time() - s
+    print(t)
+    tmpFiles(orphan = TRUE, old = TRUE, remove = TRUE)
+  }, error = function(e) {
+    print(paste("Error occurred for tile", i))
+    print(e)
+  })
 }
 
